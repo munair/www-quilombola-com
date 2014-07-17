201b84b12b69ec8989ba84ea8b175d4ea8d708a3
Munair Simpson <munair@gmail.com>
2014-07-17 09:50:07 +0000

    implementing new version

--- edit.bash
+++ edit.bash
@@ -29,8 +29,10 @@ git checkout development || git checkout -b development
 git branch
 sleep 5
 vi $1
-git add $1
+git add --all $1
 git commit -m "$2"
+git remote remove origin
+git remote add origin https://github.com/munair/www-quilombola-com.git
 git push origin development
 [ $3 == "noprompting" ] || while true; do
     read -p "shall we push changes to the staging GitHub repository and the staging instance on Heroku? " yn
@@ -47,7 +49,12 @@ sleep 5
 git merge development
 git push origin staging
 cat ~/.netrc | grep heroku || heroku login && heroku keys:add ~/.ssh/id_rsa.pub
-heroku git:remote -a staging-quilombolarecreation-kr -r staging-heroku
+git remote remove heroku
+git remote remove staging-heroku
+heroku apps:destroy dev-quilombola-com --confirm dev-quilombola-com
+heroku apps:create dev-quilombola-com
+heroku domains:add dev.quilombola.com --app dev-quilombola-com
+heroku git:remote -a dev-quilombola-com -r staging-heroku
 git push staging-heroku staging:master
 [ $3 == "noprompting" ] || while true; do
     read -p "shall we push changes to the master GitHub repository and the production instance on Heroku? " yn
@@ -62,6 +69,10 @@ git branch
 sleep 5
 git merge staging
 git push origin master
-heroku git:remote -a www-quilombolarecreation-kr -r production-heroku
+git remote remove production-heroku
+heroku apps:destroy www-quilombola-com --confirm www-quilombola-com
+heroku apps:create www-quilombola-com
+heroku domains:add www.quilombola.com --app www-quilombola-com
+heroku git:remote -a www-quilombola-com -r production-heroku
 git push production-heroku master:master
 git checkout development
