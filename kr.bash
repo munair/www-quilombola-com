5d3e8dfb673a2f3a935e5dedb6b367e4893d486f
Munair Simpson <munair@gmail.com>
2014-07-19 12:22:45 +0000

    implementing changes from recreation site

---
+++ kr.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script name : kr.bash
+# 
+git clone git@github.com:munair/www-quilombolarecreation-kr.git
+cd www-quilombolarecreation-kr
+echo "you have five seconds to Ctrl-Z in your manual changes"
+sleep 5
+cp -f ../js/* js
+cp -f ../css/* css
+cp -f ../images/* images
+cp -f ../fonts/* fonts
+cp -f ../sounds/* sounds
+cat ../README.md | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > README.md
+cat ../edit.bash | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > edit.bash
+cat ../inc_capoeira.html | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > inc_capoeira.html
+cat ../inc_music.html | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > inc_music.html
+cat ../inc_faq.html | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > inc_faq.html
+cat ../inc_contact.html | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > inc_contact.html
+cat ../inc_email.html | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > inc_email.html
+cat ../inc_formconfirmation.html | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > inc_formconfirmation.html
+cat ../package.json | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > package.json
+cat ../web.js | sed 's/recreation-com/recreation-kr/g' | sed 's/recreation\.com/recreation\.kr/g' > web.js
+bash edit.bash . "[AUTOMATED] Pushing changes to KR website from COM website excluding index.html in particular. To modify index.html git clone the repo and modify by hand." noprompting
+cd ..
+rm -rf www-quilombolarecreation-kr
